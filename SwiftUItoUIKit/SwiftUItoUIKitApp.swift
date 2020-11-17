2b953551f0b7d4ad5697e92e7dc26a5a110de784
Gio Lodi <giovanni.lodi42@gmail.com>
2020-11-17 12:57:52 +1100

    Track iOS app with SwiftUI life cycle generated by Xcode

---
+++ SwiftUItoUIKit/SwiftUItoUIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUItoUIKitApp.swift
+//  SwiftUItoUIKit
+//
+//  Created by Gio on 17/11/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUItoUIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
