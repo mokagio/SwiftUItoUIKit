7f6484133d7334f7d2f223d488370d7cfc43114b
Gio Lodi <giovanni.lodi42@gmail.com>
2020-11-17 14:02:48 +1100

    Switch from SwiftUI to UIKit App Delegate life cycle
    - Make `AppDelegate` the `@main` entry point
    - Update `UIApplicationSceneManifest` with a `UISceneConfigurations`
      pointing to "Default Configuration" (the value specified in
      `AppDelegate`) an `SceneDelegate`

--- SwiftUItoUIKit/AppDelegate.swift
+++ SwiftUItoUIKit/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 
+@main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
