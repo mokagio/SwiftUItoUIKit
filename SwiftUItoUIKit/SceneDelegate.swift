3b5fcadc2d37686ecada732e57894020dfdaada9
Gio Lodi <giovanni.lodi42@gmail.com>
2021-01-21 20:26:18 +1100

    Use `guard let` in SceneDelegate

--- SwiftUItoUIKit/SceneDelegate.swift
+++ SwiftUItoUIKit/SceneDelegate.swift
@@ -1,18 +1,18 @@
-import UIKit
 import SwiftUI
+import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+
         let contentView = ContentView()
 
-        if let windowScene = scene as? UIWindowScene {
-            let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UIHostingController(rootView: contentView)
-            self.window = window
-            window.makeKeyAndVisible()
-        }
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = UIHostingController(rootView: contentView)
+        self.window = window
+        window.makeKeyAndVisible()
     }
 }
